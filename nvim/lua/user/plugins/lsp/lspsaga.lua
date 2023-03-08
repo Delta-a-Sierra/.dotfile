85ced1361dc71f66bccfac49092ceea8083abe53
Dwayne Sutherland <dwayne@thisismother.com>
2023-03-08 10:38:27 +0000

    added lspsaga

---
+++ nvim/lua/user/plugins/lsp/lspsaga.lua
@@ -0,0 +1,8 @@
+require("lspsaga").setup({
+	"glepnir/lspsaga.nvim",
+	event = "BufRead",
+	config = function()
+		require("lspsaga").setup({})
+	end,
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+})
